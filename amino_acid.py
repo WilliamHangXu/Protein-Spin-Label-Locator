1345d49c03f35cdb939ca4c1e402a188337f0428
William_HangXu <willxh.68209@gmail.com>
2022-09-02 23:08:08 -0500

    2_Commented & Made private variables store raw data

--- amino_acid.py
+++ amino_acid.py
@@ -1,65 +1,146 @@
+
+r"""
+Class name: AminoAcid
+Description: An AminoAcid object represents an amino acid residue in a protein sequence. It records the identity, position,
+             conservation score, secondary structure assignment, and membrane affiliation.
+Variables:
+    self._num: the position of the residue in the protein
+    self._aa: the identity of the residue (single-letter amino acid name)
+    self._cons: the conservation score of the residue (from the ConSurf server)
+    self._mem: the membrane affiliation (from the Topcons server)
+    self._secstruct: the type of secondary structure the residue is in (from DSSP)
+"""
+
+
 class AminoAcid:
 
     def __init__(
             self,
             num = "",
             aa = "",
-            cons = False,
-            mem = False,
-            secstruct = False
+            cons: float = 0,
+            mem: str = "",
+            secstruct: str = ""
                  ):
+
+        r"""
+        Object constructor.
+        :param num: the position of the residue in the protein
+        :param aa: the identity of the residue (single-letter amino acid name)
+        :param cons: the conservation score of the residue (from the ConSurf server)
+        :param mem: the membrane affiliation (from the Topcons server, should be a single letter)
+        :param secstruct: the type of secondary structure the residue is in (from DSSP, should be a single letter or empty)
+        """
+
         self._num = num
         self._aa = aa
         self._cons = cons
         self._mem = mem
         self._secstruct = secstruct
 
-    def setnum(self, num):
+    def setnum(self, num: str):
+
+        r"""
+        Sets num.
+        :param num
+        :return: N/A
+        """
+
         self._num = num
 
-    def settype(self, aatype):
+    def setaa(self, aatype: str):
+
+        r"""
+        Sets amino acid identity.
+        :param aatype: single letter amino acid code
+        :return: N/A
+        """
+
         self._aa = aatype
 
-    def setcons(self):
-        self._cons = True
+    def setcons(self, cons: float):
 
-    def setmem(self):
-        self._mem = True
+        r"""
+        Sets conservation score.
+        :param cons: conservation score
+        :return: N/A
+        """
 
-    def setsecstruct(self):
-        self._secstruct = True
+        self._cons = cons
 
-    def getnum(self):
+    def setmem(self, mem: str):
+
+        r"""
+        Sets membrane affiliation information
+        :param mem: membrane affiliation
+        :return: N/A
+        """
+
+        self._mem = mem
+
+    def setsecstruct(self, secstruct):
+
+        r"""
+        Sets secondary structure information
+        :param secstruct: secondary structure
+        :return: N/A
+        """
+
+        self._secstruct = secstruct
+
+    def getnum(self) -> str:
+
+        r"""
+        Returns num.
+        :return: num
+        """
+
         return self._num
 
-    def gettype(self):
+    def getaa(self) -> str:
+
+        r"""
+        Returns aa.
+        :return: aa
+        """
+
         return self._aa
 
-    def getcons(self):
+    def getcons(self) -> float:
+
+        r"""
+        Returns cons.
+        :return: cons
+        """
+
         return self._cons
 
-    def getmem(self):
-        return self.getmem()
+    def getmem(self) -> str:
 
-    def getsecstruct(self):
+        r"""
+        Returns mem.
+        :return: mem
+        """
+
+        return self._mem
+
+    def getsecstruct(self) -> str:
+
+        r"""
+        Returns secstruct.
+        :return: secstruct
+        """
+
         return self._secstruct
 
     def aadisplay(self):
-        str = ""
-        str += self._num
-        str += self._aa
-        if self._cons:
-            str += "1"
-        else:
-            str += "0"
-        if self._mem:
-            str += "1"
-        else:
-            str += "0"
-        if self._secstruct:
-            str += "1"
-        else:
-            str += "0"
+
+        r"""
+        Prints all stored information in the amino acid object in a ordered manner.
+        :return: N/A
+        """
+
+        str = f"{self._num} {self._aa} {self._mem} {self._cons} {self._secstruct}"
         print(str)
 
 
