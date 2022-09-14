a26d2f35fbd378746af657ff759edd8890741c4b
William_HangXu <willxh.68209@gmail.com>
2022-09-14 17:22:16 -0500

    6_more functions/chain

--- amino_acid.py
+++ amino_acid.py
@@ -5,6 +5,7 @@ class AminoAcid:
     Description: An AminoAcid object represents an amino acid residue in a protein sequence. It records the identity, position,
                  conservation score, secondary structure assignment, and membrane affiliation.
     Variables:
+        self.d3to1: a dictionary that helps convert 3-letter residue names into 1-letter residue names
         self._num: the position of the residue in the protein
         self._aa: the identity of the residue (single-letter amino acid name)
         self._cons: the conservation score of the residue (from the ConSurf server)
@@ -12,9 +13,15 @@ class AminoAcid:
         self._secstruct: the type of secondary structure the residue is in (from DSSP)
     """
 
+    d3to1 = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+             'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+             'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+             'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+
     def __init__(
             self,
             num = "",
+            chain_id = "",
             aa = "",
             cons: float = 0,
             mem: str = "",
@@ -31,7 +38,8 @@ class AminoAcid:
         """
 
         self._num = num
-        self._aa = aa
+        self._chain_id = chain_id
+        self._aa = self.d3to1[aa]
         self._cons = cons
         self._mem = mem
         self._secstruct = secstruct
@@ -46,6 +54,16 @@ class AminoAcid:
 
         self._num = num
 
+    def set_chain_id(self, chainID: str):
+
+        r"""
+        Sets chain_id.
+        :param chainID
+        :return: N/A
+        """
+
+        self._chain_id = chainID
+
     def set_aa(self, aatype: str):
 
         r"""
@@ -54,7 +72,7 @@ class AminoAcid:
         :return: N/A
         """
 
-        self._aa = aatype
+        self._aa = self.d3to1[aatype]
 
     def set_cons(self, cons: float):
 
@@ -94,6 +112,15 @@ class AminoAcid:
         """
 
         return self._num
+
+    def get_chain_id(self) -> str:
+
+        r"""
+        Returns chain_id
+        :return: chain_id
+        """
+
+        return self._chain_id
 
     def get_aa(self) -> str:
 
@@ -138,7 +165,7 @@ class AminoAcid:
         :return: N/A
         """
 
-        str = f"{self._num} {self._aa} {self._mem} {self._cons} {self._secstruct}"
+        str = f"{self._num} {self._chain_id} {self._aa} {self._mem} {self._cons} {self._secstruct}"
         print(str)
 
 
